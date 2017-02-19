a2c8e92610d34b61299eb4d203aca634ec640301
Spolli <kebabnopiccante@gmail.com>
2017-02-19 11:52:18 +0100

    Update syncFolder_Robo.bat

--- syncFolder_Robo.bat
+++ syncFolder_Robo.bat
@@ -1,6 +1,6 @@
 @echo off
-rem set src = "D:\Alberto\Test\src"
-rem set dst = "D:\Alberto\Test\dst"
+rem set src = "D:\User\Test\src"
+rem set dst = "D:\User\Test\dst"
 
-robocopy "D:\Alberto\Test\src" "D:\Alberto\Test\dst" /PURGE /MIR /R:1000000 /W:30
+robocopy "D:\User\Test\src" "D:\User\Test\dst" /PURGE /MIR /R:1000000 /W:30
 pause
